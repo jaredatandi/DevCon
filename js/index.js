fd26fdbc732c885398adc9ffc31c861b2cf04b67
jaredatandi <jaredatandi07@gmail.com>
2022-11-22 17:47:28 +0300

    Implement the toggle for the popup menu from the hamburger menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-window.addEventListener('load', () => {});
+const toggleBtn = document.querySelector('.mobile-popup-nav');
+const bars = document.querySelector('.ham');
+const xicon = document.querySelector('.close');
+const links = document.querySelectorAll('.nav');
+
+let flag = false;
+
+function toggleHam() {
+  if (flag) {
+    bars.style.display = 'none';
+    toggleBtn.style.display = 'flex';
+    flag = false;
+  } else {
+    bars.style.display = 'flex';
+    toggleBtn.style.display = 'none';
+    flag = true;
+  }
+}
+bars.addEventListener('click', toggleHam);
+xicon.addEventListener('click', toggleHam);
+
+Array.from(links).forEach((link) => {
+  link.addEventListener('click', toggleHam);
+});
