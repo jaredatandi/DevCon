0181654c5effa24d3184b9e289a9d02904cbb04a
jaredatandi <jaredatandi07@gmail.com>
2022-11-23 15:06:27 +0300

    Create an object to load the content for the speakers section dynamically

--- js/index.js
+++ js/index.js
@@ -19,3 +19,56 @@ Array.from(links).forEach((link) => {
 });
 
 // Dynamic loading of speakers
+
+const speakers = {
+  data: [
+    {
+      id: 1,
+      img: './images/speaker_01.png',
+      name: 'Jefferson Lakuel',
+      career: 'Google Developer Expert',
+      description:
+        'Has been into Angular for the past years and loves to share his knowledge about Angular through conferences.',
+    },
+    {
+      id: 2,
+      img: './images/speaker_03.png',
+      name: 'Rachel Maria',
+      career: 'Google Developer Expert',
+      description:
+        'Has been into Angular for the past years and loves to share his knowledge about Angular through conferences.',
+    },
+  ],
+};
+
+let speakersSectionMain = `
+      <section id="speakers" class="speakers dflex">
+
+        <div class="header lato">
+          <h2>Featured Speakers</h2>
+          <div class="line"></div>
+        </div>
+        <div class="speakers-cards dflex">
+        `;
+
+speakers.data.forEach((speaker) => {
+  speakersSectionMain += `
+            <div class="speakers-content dflex">    
+            <div class="speaker-img">
+              <img src="${speaker.img}" alt="speaker_01" />
+            </div>
+            <div class="speaker-des dflex lato">
+              <h3>${speaker.name}</h3>
+              <p class="career">${speaker.career}</p>
+              <div class="seperator"></div>
+              <p class="work">
+              ${speaker.description}
+              </p>
+            </div>
+          </div>
+          `;
+});
+
+speakersSectionMain += '</div> </div> </section>';
+
+document.querySelector('.speakers-section').innerHTML = speakersSectionMain;
